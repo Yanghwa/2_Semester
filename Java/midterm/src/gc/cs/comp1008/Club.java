aaad6b3400cf2bc5d322b10f127d092b8cc0961f
Yang Junghwan <kellerj87@gmail.com>
2016-07-04 14:05:31 -0400

    Java midterm updated version

--- Java/midterm/src/gc/cs/comp1008/Club.java
+++ Java/midterm/src/gc/cs/comp1008/Club.java
@@ -2,143 +2,201 @@ package gc.cs.comp1008;
 
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
- * This class models the details of a club with memberships. A club object consists of members
- * that are modelled by the Membership class
+ * This class models the details of a club with memberships. A club object
+ * consists of members that are modelled by the Membership class
  *
  * @author Anju Chawla Junghwan Yang
  * @version June 20 2016
  */
-public class Club
-{
-   private final String clubName;
+public class Club {
+	private final String clubName;
 
-   //add an instance variable to hold the list of members in the club. These members are objects of type Membership
-   private ArrayList<Membership> clubMember = new ArrayList();
+	// add an instance variable to hold the list of members in the club. These
+	// members are objects of type Membership
+	private ArrayList<Membership> clubMember = new ArrayList();
+	// private ArrayList<Membership> members;
 
-
-
- /**
-  * This method returns arraylist clubmember
- * @return the clubMember
- */
+	/**
+	 * This method returns arraylist clubmember
+	 * 
+	 * @return the clubMember
+	 */
 	public ArrayList<Membership> getClubMember() {
 		return clubMember;
+		// members = new ArrayList<Membership>();
 	}
-	
+
 	/**
 	 * This method sets clubmember arraylist
-	 * @param clubMember the clubMember to set
+	 * 
+	 * @param clubMember
+	 *            the clubMember to set
 	 */
 	public void setClubMember(ArrayList<Membership> clubMember) {
 		this.clubMember = clubMember;
 	}
 
 	/**
-        * The constructor for objects of class Club
-        * @name the name of the club
-        */
-   		//Complete the constructor body
+	 * The constructor for objects of class Club
+	 * 
+	 * @name the name of the club
+	 */
+	// Complete the constructor body
 
-   public Club(String name)
-   {
-       this.clubName = name;
-       
+	public Club(String name) {
+		this.clubName = name;
+	}
 
-   }
+	/**
+	 * This method returns the name of the club
+	 * 
+	 * @return The name of the club
+	 */
+	public String getClubName() {
+		return clubName;
+	}
 
-   /**
-   This method returns the name of the club
-   @return The name of the club
-   */
-   public String getClubName()
-   {
-   	return clubName;
-   }
+	/**
+	 * Add a new member to the club's list of members.
+	 * 
+	 * @param member
+	 *            the member to be given the membership for the club, cannot
+	 *            have the same member id as a previous member
+	 * @return true, if membership granted, else false
+	 */
+	// Complete this method, it should 'use' the equals method of the Membership
+	// class
+	public boolean join(Membership member) {
+		if (member != null) {
+			if (!clubMember.contains(member)) {
+				clubMember.add(member);
+				return true;
+			}
+		}
+		return false;
+		// This way is not using equals;override in membership
+		// if (clubMember == null) {
+		// clubMember.add(member);
+		// return true;
+		// } else {
+		// for (Membership mem : clubMember) {
+		// if (mem.equals(member)) {
+		// return false;
+		// } else
+		// continue;
+		// }
+		// clubMember.add(member);
+		// return true;
+		// }
+		// } else
+		// return false;
 
-    /**
-     * Add a new member to the club's list of members.
-     * @param member the member to be given the membership for the club, cannot have the same member id as a previous member
-     * @return true, if membership granted, else false
-     */
-     //Complete this method, it should 'use' the equals method of the Membership class
-    public boolean join(Membership member)
-    {
-    	if(member != null){
-    		if (clubMember == null) {
-    			clubMember.add(member);
-    			return true;
-    		}
-    		else {
-	    		for(Membership mem : clubMember) {
-	    			if (mem.equals(member)) {
-	    				return false;
-	    			}
-	    			else continue;
-	    		}
-	    		clubMember.add(member);
-	    		return true;
-    		}
-    	}
-    	else return false;
+	}
 
-    }
+	/**
+	 * This method returns the number of members in the club
+	 * 
+	 * @return The number of members in the club.
+	 */
+	// Complete this method
 
-    /**
-     * This method returns the number of members in the club
-     * @return The number of members in the club.
-     */
-  //Complete this method
+	public int numberOfMembers() {
+		return clubMember.size();
+	}
 
-    public int numberOfMembers()
-    {
-    	return clubMember.size();
-    }
+	/**
+	 * This method returns the number of members who joined the club in the
+	 * provided month and year
+	 * 
+	 * @param month
+	 *            the month that the membership was taken
+	 * @param the
+	 *            year the membership was taken
+	 * @return The number of members who joined the club in the provided month
+	 *         and year
+	 *
+	 */
+	public int noOfMembersJoinedClub(Month month, int year) {
+		// this way is making new list, but it doesn't have to do
+		// ArrayList<Membership> joinedClub = new ArrayList();
+		// for (Membership mem : clubMember) {
+		// if (mem.getMonth().equals(month) && mem.getYear() == year) {
+		// joinedClub.add(mem);
+		// } else
+		// continue;
+		// }
+		// return joinedClub.size();
 
-    /**
-     * This method returns the number of members who joined the club in the provided month and year
-     * @param month the month that the membership was taken
-     * @param the year the membership was taken
-     * @return The number of members who joined the club in the provided month and year
-     *
-     */
-    public int noOfMembersJoinedClub(Month month, int year)
-    {	
-    	ArrayList<Membership> joinedClub = new ArrayList();
-    	for(Membership mem : clubMember) {
-    		if (mem.getMonth().equals(month) && mem.getYear() == year) {
-    			joinedClub.add(mem);
-    		}
-    		else continue;
-    	}
-    	return joinedClub.size();
-    }
+		int count = 0;
+		// for(int i = 0;i <= clubMember.size(); i++) {
+		//
+		// }
+		for (Membership m : clubMember) {
+			if (m.getYear() == year && m.getMonth().equals(month)) {
+				count++;
+			}
+		}
+		return count;
+		// return membersJoinedClub(month, year).size();
+	}
 
-    /**
-     * This method returns the list of members who joined the club in the provided month and year
-     * @param month the month that the membership was taken
-     * @param the year the membership was taken
-     * @return The list of members who joined the club in the given month and year
-     *
-     */
-    //Code this method
-    public ArrayList MembersJoinedClub(Month month, int year)
-    {
-    	ArrayList<Membership> joinedClub = new ArrayList();
-//    	System.out.println("Here is their name and id: ");
-    	for(Membership mem : clubMember) {
-    		if (mem.getMonth().equals(month) && mem.getYear() == year) {
-    			joinedClub.add(mem);
-//    			System.out.printf("%s	%s%n", mem.getMemberName(), mem.getMemberId());
-    		}
-    		else continue;
-    	}
-    	this.clubMember = joinedClub;
-    	return this.clubMember;
-    	
-    }
+	/**
+	 * This method returns the list of members who joined the club in the
+	 * provided month and year
+	 * 
+	 * @param month
+	 *            the month that the membership was taken
+	 * @param the
+	 *            year the membership was taken
+	 * @return The list of members who joined the club in the given month and
+	 *         year
+	 *
+	 */
+	// Code this method
+	public ArrayList<Membership> membersJoinedClub(Month month, int year) {
+		ArrayList<Membership> joinedClub = new ArrayList<Membership>();
+		for (Membership mem : clubMember) {
+			if (mem.getMonth().equals(month) && mem.getYear() == year) {
+				joinedClub.add(mem);
+			}
+		}
+		return joinedClub;
 
+	}
 
-   }//of Club
+	/**
+	 * This method removes the members who joined the club in the given month
+	 * and year
+	 * 
+	 * @param month
+	 *            the month that the membership was taken
+	 * @param year
+	 *            the year the membership was takedn
+	 * @return the list of memberships that were revoked in the given month and
+	 *         year
+	 */
+	public ArrayList<Membership> deletedMembers(Month month, int year) {
+		ArrayList<Membership> deleted = new ArrayList<Membership>();
+		Iterator<Membership> it = clubMember.iterator();
+		Membership m;
+		while (it.hasNext()) {
+			m = it.next();
+			if (m.getMonth().equals(month) && m.getYear() == year) {
+				deleted.add(m);
+				it.remove();
+			}
+		}
+		//
+		// for (Membership mem : clubMember) {
+		// if (mem.getMonth().equals(month) && mem.getYear() == year) {
+		// clubMember.remove(mem);
+		// deleted.add(mem);
+		// }
+		// }
+		return deleted;
 
+	}
+}// of Club
--- Java/midterm/src/gc/cs/comp1008/Tester.java
+++ Java/midterm/src/gc/cs/comp1008/Tester.java
@@ -1,4 +1,5 @@
 package gc.cs.comp1008;
+
 /**
  * This class tests the Club and Membership classes
  * @author Anju Chawla Junghwan Yang
@@ -11,7 +12,6 @@ import java.util.ArrayList;
 public class Tester {
 
 	public static void main(String[] args) {
-
 
 		Membership m1 = new Membership("1", "Anju Chawla", YearMonth.parse("2007-08"));
 		Membership m2 = new Membership("2", "Jaret Wright", YearMonth.of(2010, 1));
@@ -30,67 +30,71 @@ public class Tester {
 
 		System.out.println("------------------------------------------------------------------------------------");
 
-		//1) complete this statement to print the club name and the number of members in it
-		System.out.printf("Total Numbers of members in the %s club: %d%n", instructorClub.getClubName(),instructorClub.numberOfMembers());
+		// 1) complete this statement to print the club name and the number of
+		// members in it
+		System.out.printf("Total Numbers of members in the %s club: %d%n", instructorClub.getClubName(),
+				instructorClub.numberOfMembers());
 		System.out.println("------------------------------------------------------------------------------------");
 
-		//2) complete this statement to print the number of members who joined the club in March of 2010
-		System.out.printf("Number of members who joined in March, 2010: %d%n", instructorClub.noOfMembersJoinedClub(Month.MARCH, 2010));
+		// 2) complete this statement to print the number of members who joined
+		// the club in March of 2010
+		System.out.printf("Number of members who joined in March, 2010: %d%n",
+				instructorClub.noOfMembersJoinedClub(Month.MARCH, 2010));
 
-		//3)  now  call the method membersJoinedClub to get the list of the members who joined the club in March of 2010
-		ArrayList<Membership> a = instructorClub.MembersJoinedClub(Month.MARCH, 2010);
-//		Club a = instructorClub.MembersJoinedClub(Month.MARCH, 2010);
+		// 3) now call the method membersJoinedClub to get the list of the
+		// members who joined the club in March of 2010
+		ArrayList<Membership> a = instructorClub.membersJoinedClub(Month.MARCH, 2010);
+
+		// 4) call the helper method displayMembers to display this list of
+		// members
 		displayMembers(a);
-
-		//4) call the helper method displayMembers to display this list of members
-		
-		
 
 		System.out.println("------------------------------------------------------------------------------------");
 
-
-		//5a) complete this statement to print the number of members who joined the club in May of 2010
-		System.out.printf("Number of members who joined in May, 2010: %d%n", instructorClub.noOfMembersJoinedClub(Month.MAY, 2010));
-		//5b)  now  call the method membersJoinedClub to get the list of the members who joined the club in May of 2010
-		a = instructorClub.MembersJoinedClub(Month.MAY, 2010);
-//		Club b = instructorClub.MembersJoinedClub(Month.MAY, 2010);
-		//6) call the helper method displayMembers to display this list of members
+		// 5a) complete this statement to print the number of members who joined
+		// the club in May of 2010
+		System.out.printf("Number of members who joined in May, 2010: %d%n",
+				instructorClub.noOfMembersJoinedClub(Month.MAY, 2010));
+		// 5b) now call the method membersJoinedClub to get the list of the
+		// members who joined the club in May of 2010
+		a = instructorClub.membersJoinedClub(Month.MAY, 2010);
+		// 6) call the helper method displayMembers to display this list of
+		// members
 		displayMembers(a);
-//		displayMembers(b);
-
+		System.out.println("------------------------------------------------------------------------------------");
+		a = instructorClub.deletedMembers(Month.MARCH, 2010);
+		System.out.println("These members are leaving the club");
+		displayMembers(a);
 
 	}// of main()
 
 	/*
-	 * 7) Create a helper method that takes a list of members as an argument.It displays the name and id of each member
-	 * by calling the methods of the Membership class
-	 * DISPLAY ONLY IF THE LIST IS NOT EMPTY - display an appropriate heading like "Here is their name and id: "
-	 * followed by the name and id of each member, one member per line, the two values separated by a tab or comma
-	 * IF THE LIST IS EMPTY - there in no output
-	*/
+	 * 7) Create a helper method that takes a list of members as an argument.It
+	 * displays the name and id of each member by calling the methods of the
+	 * Membership class DISPLAY ONLY IF THE LIST IS NOT EMPTY - display an
+	 * appropriate heading like "Here is their name and id: " followed by the
+	 * name and id of each member, one member per line, the two values separated
+	 * by a tab or comma IF THE LIST IS EMPTY - there in no output
+	 */
 	public static void displayMembers(ArrayList<Membership> a) {
-		
-		if (a.size() == 0) {
-			
-		}
-		else {
+
+		if (a.size() > 0) {
 			System.out.println("Here is their name and id: ");
-			for (Membership m: a) {
-				System.out.printf("%s	%s%n", m.getMemberName(), m.getMemberId());
+			for (Membership m : a) {
+				System.out.printf("%s\t%s%n", m.getMemberName(), m.getMemberId());
 			}
-			
 		}
-		
-		
 	}
-	//Code this method
-
-
+	// Code this method
 
 	/**
-	 * This helper method lets a member with a unique id join the club, else denies the membership
-	 * @param c the club whose membership is being sought
-	 * @param m the member who wants to join the club
+	 * This helper method lets a member with a unique id join the club, else
+	 * denies the membership
+	 * 
+	 * @param c
+	 *            the club whose membership is being sought
+	 * @param m
+	 *            the member who wants to join the club
 	 */
 	private static void joinTheClub(Club c, Membership m) {
 
